13b0b0def7824e55123398bbf1e030be3721cf94
JORGE <jorge@localhost.localdomain>
2017-11-20 16:57:14 -0600

    update controller seguridad

--- js/seguridadCtrl.js
+++ js/seguridadCtrl.js
@@ -5,9 +5,41 @@ app.controller('seguridadCtrl',['$scope',function($scope){
 app.controller('info',function($scope,$http){
   $scope.topics = {};
   $scope.section = "Holas";
-  $http.get('json/indice.json').success(function(data){
-    $scope.topics = data.topics;
-  });
+  
+  $scope.topics = [
+    {
+      "title" : "Titulo A",
+      "summary" : "orem ipsum dolor sit amet, consectetur adipisicing elit. Expedita voluptatum ullam, possimus aliquam mollitia, inventore ipsam quaerat atque. Corporis, excepturi.",
+      "labels"  : "basico"
+    },
+    {
+      "title" : "Titulo B",
+      "summary" : "orem ipsum dolor sit amet, consectetur adipisicing elit. Expedita voluptatum ullam, possimus aliquam mollitia, inventore ipsam quaerat atque. Corporis, excepturi.",
+      "labels"  : "basico"
+    },
+    {
+      "title" : "Titulo C",
+      "summary" : "orem ipsum dolor sit amet, consectetur adipisicing elit. Expedita voluptatum ullam, possimus aliquam mollitia, inventore ipsam quaerat atque. Corporis, excepturi.",
+      "labels"  : "basico"
+    },
+    {
+      "title" : "Titulo A",
+      "summary" : "orem ipsum dolor sit amet, consectetur adipisicing elit. Expedita voluptatum ullam, possimus aliquam mollitia, inventore ipsam quaerat atque. Corporis, excepturi.",
+      "labels"  : "basico"
+    },
+    {
+      "title" : "Titulo C",
+      "summary" : "orem ipsum dolor sit amet, consectetur adipisicing elit. Expedita voluptatum ullam, possimus aliquam mollitia, inventore ipsam quaerat atque. Corporis, excepturi.",
+      "labels"  : "basico"
+    },
+    {
+      "title" : "Titulo E",
+      "summary" : "orem ipsum dolor sit amet, consectetur adipisicing elit. Expedita voluptatum ullam, possimus aliquam mollitia, inventore ipsam quaerat atque. Corporis, excepturi.",
+      "labels"  : "basico"
+    }
+  ]
+
+
   $scope.guia = function(){ $scope.section = "Guias"}
   $scope.siguiente = function(){  }
 });
