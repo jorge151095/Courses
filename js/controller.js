bd6d5ed66ac10596ffe54b9d574b235be1ddcd77
JORGE <jorge@localhost.localdomain>
2017-11-19 22:03:15 -0600

    list p2

--- js/controller.js
+++ js/controller.js
@@ -48,11 +48,40 @@ app.config(function($routeProvider) {
 app.controller('mainController', function($scope,$mdDialog) {
     $scope.urlvideo = "video/B1.mp4";
     $scope.bs1 = [
-        { name: 'Pepperoni', url: '' },
-        { name: 'Sausage', url: '' },
-        { name: 'Black Olives', url: '' },
-        { name: 'Green Peppers', url: '' }
-      ];
+        { name: '\t    1.1 ¿Qué es la seguridad informática?', url: '' },
+        { name: '\t 1.2 Principales amenazas de seguridad informática', url: '' },
+        { name: '\t 1.3 Medidas indispensables para tu seguridad informática', url: '' }
+    ];
+    $scope.bs2 = [
+        { name: '2.1 Conceptos de seguridad informática', url: '' },
+        { name: '2.2 Seguridad en la información', url: '' },
+        { name: '2.3 Principios de seguridad', url: '' }
+    ];
+    $scope.bs3 = [
+        { name: '3.1 Secreto de confidencialidad', url: '' },
+        { name: '3.2 Integridad', url: '' },
+        { name: '3.3 Accesibilidad o disponibilidad', url: '' },
+        { name: '3.4 Autenticidad', url: '' },
+        { name: '3.5 No repudio', url: '' },
+        { name: '3.6 Consistencia', url: '' },
+        { name: '3.7 Aislamiento', url: '' },
+        { name: '3.8 Auditoria', url: '' },
+        { name: '3.9 Prevención e información', url: '' }
+    ];
+    $scope.bs4 = [
+        { name: '4.1 Como sentirse seguro al navegar', url: '' },
+        { name: '4.2 Navegadores web', url: '' },
+        { name: '4.3 Contrafuegos (Firewall)', url: '' },
+        { name: '4.4 Correo electrónico', url: '' },
+        { name: '4.5 Redes sociales', url: '' }
+    ];
+    $scope.bs5 = [
+        { name: '5.1 El virus en acción', url: '' },
+        { name: '5.2 ¿Qué tipo de virus existen?', url: '' },
+        { name: '5.3 La prevención - antivirus', url: '' },
+        { name: '5.4 El antivirus en acción', url: '' }
+    ];
+      
     /*$scope.showAdvanced = function(ev) {
       $mdDialog.show({
         controller: DialogController,
