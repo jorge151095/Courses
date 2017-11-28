786e30b72a0db2b111c9158852a405c48b97713e
JORGE <jorge@localhost.localdomain>
2017-11-27 18:54:07 -0600

    avances

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -5,55 +5,26 @@ app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
     $scope.noWrapSlides = false;
     $scope.active = 0;
     var slides = $scope.slides = [];
-    var currIndex = 0;
   
-    $scope.addSlide = function() {
-      var newWidth = 600 + slides.length + 1;
-      slides.push({
-        image: 'https://unsplash.it/' + newWidth + '/300',
-        text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
-        id: currIndex++
-      });
-    };
-  
-    $scope.randomize = function() {
-      var indexes = generateIndexesArray();
-      assignNewIndexesToSlides(indexes);
-    };
-  
-    for (var i = 0; i < 4; i++) {
-      $scope.addSlide();
-    }
-  
-    // Randomize logic below
-  
-    function assignNewIndexesToSlides(indexes) {
-      for (var i = 0, l = slides.length; i < l; i++) {
-        slides[i].id = indexes.pop();
-      }
-    }
-  
-    function generateIndexesArray() {
-      var indexes = [];
-      for (var i = 0; i < currIndex; ++i) {
-        indexes[i] = i;
-      }
-      return shuffle(indexes);
-    }
-  
-    // http://stackoverflow.com/questions/962802#962890
-    function shuffle(array) {
-      var tmp, current, top = array.length;
-  
-      if (top) {
-        while (--top) {
-          current = Math.floor(Math.random() * (top + 1));
-          tmp = array[current];
-          array[current] = array[top];
-          array[top] = tmp;
-        }
-      }
-  
-      return array;
-    }
+    slides.push({
+      image:'img/B1.jpeg',
+      text: 'welcome 1',
+      title: 'Básico',
+      id: 0
+    });
+
+    slides.push({
+      image:'img/B2.jpeg',
+      text: 'welcome 2',
+      title: 'Intermedio',
+      id: 1
+    });
+
+    slides.push({
+      image:'img/C2.jpeg',
+      text: 'welcome 3',
+      title:'Avanzado',
+      id: 2
+    });
+ 
 }]);
--- js/controllers/seguridadCtrl.js
+++ js/controllers/seguridadCtrl.js
@@ -33,6 +33,77 @@ app.controller('seguridadCtrl',function($scope){
             { name: '5.3 La prevención  antivirus', url: '' },
             { name: '5.4 El antivirus en acción', url: '' }
         ];
+    
+        $scope.is1 = [
+            { name: '1.1 Las buenas prácticas que no siempre se cumplen', url: '' }
+        ];
+        $scope.is2 = [
+            { name: '2.1 Los protagonistas (Hacker, Cracker, entre otros)', url: '' },
+            { name: '2.2 Fuentes confiables', url: '' }
+        ];
+        $scope.is3 = [
+            { name: '3.1 Tipos de vulnerabilidad', url: '' },
+            { name: '3.2 Medidas preventivas', url: '' }
+        ];
+        $scope.is4 = [
+            { name: '4.1 Ataques activos', url: '' },
+            { name: '4.2 Ataques pasivos', url: '' }
+        ];
+        $scope.is5 = [
+            { name: '5.1 Adware y spyware en funcionamiento', url: '' },
+            { name: '5.2 Diferencias entre adware, spywate y virus', url: '' },
+            { name: '5.3 Limpiar y proteger el equipo', url: '' }
+        ];
+        
+        $scope.is6 = [
+            { name: '6.1 Prinicipo de menor privilegio', url: '' },
+            { name: '6.2 Oscuridad no equivalente a seguridad', url: '' },
+            { name: '6.3 Principio del eslabón más débil', url: '' },
+            { name: '6.4 Defensa en profundidad', url: '' },
+            { name: '6.5 Punto de control centralizado', url: '' },
+            { name: '6.6 Seguridad en caso de fallo', url: '' },
+            { name: '6.7 Partición universal', url: '' },
+            { name: '6.8 Principio de simplicidad', url: '' }
+        ];
+
+        $scope.as1 = [
+            { name: '1.1 Anatomía de un ataque', url: '' },
+            { name: '1.2 La seguridad en las empresas', url: '' }
+        ];
+        $scope.as2 = [
+            { name: '2.1 ¿Cómo se relaciona Ingeniería social con seguridad informática?', url: '' },
+            { name: '2.2 Phising', url: '' },
+            { name: '2.3 Baiting', url: '' },
+            { name: '2.4 Vishing', url: '' }
+        ];
+        $scope.as3 = [
+            { name: '3.1 Windows vs Linux', url: '' },
+            { name: '3.2 Un sistema operativo seguro', url: '' },
+            { name: '3.3 Consejos para sistemas Windows', url: '' },
+            { name: '3.4 Consejos para sistemas Unix', url: '' }
+        ];
+        $scope.as4 = [
+            { name: '4.1 Protección imprescindible', url: '' },
+            { name: '4.2 Windows Firewall', url: '' },
+            { name: '4.3 Excepciones', url: '' }
+        ];
+        $scope.as5 = [
+            { name: '5.1 Los problemas de las contraseñas', url: '' },
+            { name: '5.2 Configuración básica', url: '' }
+        ];
+        $scope.as6 = [
+            { name: '6.1 La privacidad', url: '' },
+            { name: '6.2 Datos personales', url: '' },
+            { name: '6.3 Encriptación', url: '' },
+            { name: '6.4 ¿Qué es una cookie?', url: '' }
+        ];
+        $scope.as7 = [
+            { name: '7.1 El autoconocimiento', url: '' },
+            { name: '7.2 Amenazas y vulnerabilidades', url: '' },
+            { name: '7.3 Implementación de controles', url: '' },
+            { name: '7.4 Políticas y otros documentos', url: '' },
+            { name: '7.4 Responsabilidades', url: '' }
+        ];
 
   $scope.guia = function(){ $scope.section = "Guias"}
   $scope.siguiente = function(){  }
